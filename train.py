e2382c13e7b9fbff999e77207c41bc5d0da1a1e8
nk7260ynpa <nk7260ynpa@gmail.com>
2022-08-06 23:57:30 +0800

    Complete Train Predict

---
+++ train.py
@@ -0,0 +1,106 @@
+import os 
+import datetime
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision as trv
+from PIL import Image
+import random
+import utils
+import logging
+import argparse
+
+def main(opt):
+    VOC_DIR = opt.data_folder
+    BATCH_SIZE = opt.batch_size
+    NUM_CLASSES = opt.num_classes
+    NUM_EPOCHS = opt.epochs
+    Learning_Rate = opt.lr
+    Weight_Decay = opt.Weight_decay
+    
+    log_name = datetime.datetime.strftime(datetime.datetime.now(), "%m%d%H%M") + ".log"
+    log_path = os.path.join("logs", log_name)
+    logging.basicConfig(level=logging.DEBUG, filename=log_path, filemode='w', format="")
+    weights_name = datetime.datetime.strftime(datetime.datetime.now(), "%m%d%H%M") + ".pth"
+    save_path = os.path.join("weights", weights_name)
+    device = torch.device("cuda" if torch.cuda.is_available else "cpu")
+
+    Train_dataset = utils.VOC_dataset(VOC_DIR)
+    Valid_dataset = utils.VOC_dataset(VOC_DIR, train=False)
+
+    Train_loader = torch.utils.data.DataLoader(Train_dataset, BATCH_SIZE, shuffle=True, 
+                                               drop_last=True, num_workers=4)
+    Valid_loader = torch.utils.data.DataLoader(Valid_dataset, BATCH_SIZE, shuffle=False, 
+                                               drop_last=True, num_workers=4)
+
+    model = utils.ResNet18_FCN(NUM_CLASSES)
+
+    def loss_fun(inputs, targets):
+        return F.cross_entropy(inputs, targets, reduction="none").mean(1).mean(1)
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=Learning_Rate, weight_decay=Weight_Decay)
+
+    model.to(device);
+
+    for epoch in range(NUM_EPOCHS):
+
+        print(f"Epoch: {epoch+1} ")
+        logging.info(f"Epoch: {epoch+1} ")
+
+        model.train()
+        train_loss = 0.0
+        sample_num = 0.0
+        for inputs, labels in Train_loader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            loss = loss_fun(outputs, labels).sum()
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
+            sample_num += labels.shape[0]
+
+        train_loss = train_loss / sample_num
+        print(f'train Loss: {train_loss:.4f}')
+        logging.info(f'train Loss: {train_loss:.4f}')
+
+        model.eval()
+        valid_loss = 0.0
+        sample_num = 0.0
+        for inputs, labels in Valid_loader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            with torch.no_grad():
+                outputs = model(inputs)
+                _, preds = torch.max(outputs, 1)
+                loss = loss_fun(outputs, labels).sum()
+                valid_loss += loss.item()
+                sample_num += labels.shape[0]
+
+        valid_loss = valid_loss / sample_num
+        print(f'valid Loss: {valid_loss:.4f}')
+        logging.info(f'valid Loss: {valid_loss:.4f}')
+    
+    model.to("cpu");
+    torch.save(model.state_dict(), save_path)
+    
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_folder', type=str, default="dataset/VOCdevkit/VOC2012/", help="dataset path")
+    parser.add_argument('--batch_size', type=int, default=16, help="batch size")
+    parser.add_argument('--num_classes', type=int, default=21, help="number of all classes")
+    parser.add_argument('--epochs', type=int, default=30, help='Interval between each validation')
+    parser.add_argument('--saved_model', default='', help="epochs")
+    parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+    parser.add_argument('--Weight_decay', type=float, default=1e-3, help='weight decay')
+    
+    opt = parser.parse_args()
+    main(opt)
--- utils.py
+++ utils.py
@@ -8,6 +8,31 @@ import torchvision as trv
 from PIL import Image
 import random
 
+VOC_COLORMAP = [[0, 0, 0], [128, 0, 0], [0, 128, 0], [128, 128, 0],
+                [0, 0, 128], [128, 0, 128], [0, 128, 128], [128, 128, 128],
+                [64, 0, 0], [192, 0, 0], [64, 128, 0], [192, 128, 0],
+                [64, 0, 128], [192, 0, 128], [64, 128, 128], [192, 128, 128],
+                [0, 64, 0], [128, 64, 0], [0, 192, 0], [128, 192, 0], [0, 64, 128]]
+
+def label2image(pred):
+    colormap = torch.tensor(VOC_COLORMAP) 
+    X = pred.long() 
+    return colormap[X, :].numpy()
+
+def bilinear_kernel(in_channels, out_channels, kernel_size):
+    factor = (kernel_size + 1) // 2
+    if kernel_size % 2 == 1:
+        center = factor - 1.
+    else:
+        center = factor - 0.5
+    
+    og = (torch.arange(kernel_size).reshape(-1, 1),
+          torch.arange(kernel_size).reshape(1, -1))
+    filt = (1 - torch.abs(og[0] - center) / factor) * (1 - torch.abs(og[1] - center) / factor)
+    weight = torch.zeros((in_channels, out_channels, kernel_size, kernel_size))
+    weight[range(in_channels), range(out_channels), :, :] = filt
+    return weight
+
 class VOC_dataset(torch.utils.data.Dataset):
     def __init__(self, VOC_DIR, train=True, crop_size=(320, 480)):
         self.voc_dir = VOC_DIR
@@ -84,3 +109,31 @@ class VOC_dataset(torch.utils.data.Dataset):
             axes[i+num_cols].axes.get_yaxis().set_visible(False)
             
         plt.show()
+        
+
+class ResNet18_FCN(nn.Module):
+    def __init__(self, NUM_CLASSES):
+        super(ResNet18_FCN, self).__init__()
+        self.num_classes = NUM_CLASSES
+        self.Conv_base = self.build_ResNet18()
+        self.final_conv = nn.Conv2d(512, NUM_CLASSES, kernel_size=1)
+        self.transpose_conv = nn.ConvTranspose2d(NUM_CLASSES, NUM_CLASSES, 
+                                                 kernel_size=64, padding=16, stride=32)
+        self.weight_init()
+    
+    def build_ResNet18(self):
+        resnet18 = trv.models.resnet18(weights=trv.models.ResNet18_Weights.IMAGENET1K_V1)
+        Conv_base = nn.Sequential(*list(resnet18.children())[:-2])
+        return Conv_base
+    
+    def weight_init(self):
+        torch.nn.init.xavier_uniform_(self.final_conv.weight)
+        W = bilinear_kernel(self.num_classes, self.num_classes, 64)
+        self.transpose_conv.weight.data.copy_(W)
+        
+    def forward(self, img):
+        x = self.Conv_base(img)
+        x = self.final_conv(x)
+        x = self.transpose_conv(x)
+        return x
+
